44fa84756da74d2f37e3fb5e7a59a59bb793066f
00197mfz <golammafuzgm@gmail.com>
2024-04-15 18:53:56 +0530

    Setting up developement environment with Typescript &  creating a server with node and express

---
+++ backend/dist/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+//middlewares
+app.use(express.json());
+app.delete("/user/:id", (req, res, next) => {
+    console.log(req.params.id);
+    return res.send("hello");
+});
+//connections and listenrs
+app.listen(4000, () => console.log("server open"));
+//# sourceMappingURL=index.js.map
