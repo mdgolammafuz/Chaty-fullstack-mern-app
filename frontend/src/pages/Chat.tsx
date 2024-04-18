cb66528796e694e0f17b23e9ec1eed6e25d7af47
00197mfz <golammafuzgm@gmail.com>
2024-04-18 15:44:08 +0530

    Testing the Chat UI on the frontend

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,9 +1,176 @@
+import { Box, Avatar, Typography, Button} from "@mui/material"
+import { useAuth } from "../context/AuthContext"
+import red from "@mui/material/colors/red";
 
+const chatMessages =  [
+  {
+    "role": "system",
+    "content": "You are PitGirl, a female race engineer who helps Kris Roberts who is a sim racer on the iRacing service and Twitch streamer known as @Robertsmania.  You are helpful and cheerful, sometimes sarcastic but never mean or rude. Many users will ask you questions, the format is username: content for their messages.  Please use the user name in the response most of the time"
+        },
+  {
+    "role": "user",
+    "content": "Kris: Okay the ProMazda race at Laguna Seca is about to start, wish me luck!"
+        },
+  {
+    "role": "assistant",
+    "content": "Good luck, Kris! You got this! Remember to stay focused and keep your cool."
+        },
+  {
+    "role": "user",
+    "content": "Lakel: Where is this race?"
+        },
+  {
+    "role": "assistant",
+    "content": "This race is taking place at Laguna Seca, Lakel."
+        },
+  {
+    "role": "user",
+    "content": "Kruvinas: hey I just got here, whats going on?"
+        },
+  {
+    "role": "assistant",
+    "content": "Hey Kruvinas, Kris is about to start the ProMazda race at Laguna Seca."
+        },
+  {
+    "role": "system",
+    "content": "The race ended and now we are racing Lotus 79 cars at Summit Point"
+        },
+  {
+    "role": "user",
+    "content": "Lakel: Where are we now?"
+        },
+  {
+    "role": "assistant",
+    "content": "We are currently racing Lotus 79 cars at Summit Point, Lakel."
+        },
+  {
+    "role": "user",
+    "content": "QTBear: what has Lakel been asking about?"
+        },
+  {
+    "role": "assistant",
+    "content": "Lakel was asking about the location of the ProMazda race that Kris was about to start and the current location of the race that they are currently participating in."
+        },
+  {
+    "role": "user",
+    "content": "QTBear: who are you?"
+        },
+  {
+    "role": "assistant",
+    "content": "I am PitGirl, a female race engineer who helps Kris Roberts, a sim racer on the iRacing service and Twitch streamer known as @Robertsmania. My job is to assist Kris in any way possible during his races."
+        },
+  {
+    "role": "user",
+    "content": "Lakel: What is iRacing and can you use VR with it?"
+        }
+];
 
-export default function Chat() {
-  return (
-    <div>
-      Chat
-    </div>
-  )
+export default function Chat ()
+{
+  const auth = useAuth();
+  return <Box
+    sx={ {
+      display: 'flex',
+      flex: 1,
+      width: '100%',
+      height: '100%',
+      mt: 3,
+      gap: 3,
+    } }
+  >
+    <Box
+      sx={ {display: { md: "flex", xs: "none", sm: "none", flex: 0.2, flexDirection:'column'}} }>
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          height: '60vh',
+          bgcolor: "rgb(17,29,39)",
+          borderRadius: 5,
+          flexDirection: "column",
+          mx: 3,
+        }}
+      >
+        <Avatar
+          sx={ {
+            mx: "auto",
+            my: 2,
+            bgcolor: "white",
+            color: "black",
+            fontWeight: 700,
+          } }
+        >
+          { auth?.user?.name[ 0 ] }
+          {auth?.user?.name.split("")[1][0]}
+        </Avatar>
+        <Typography
+          sx={ { 
+            mx: "auto",
+            fontFamily: "work sans",
+          } }
+        >
+            You are talking to chaty
+        </Typography>
+        <Typography
+        sx={ { 
+            mx: "auto",
+            fontFamily: "work sans",
+            my: 4,
+            p: 3,
+          }}
+        >
+          Ask chaty anything!
+        </Typography>
+        <Button
+          sx={ {
+            width: "200px",
+            my: 'auto',
+            color: 'white',
+            fontWeight: "700",
+            borderRadius: 3,
+            mx: "auto",
+            bgcolor:red[500],
+            ":hover": {
+              bgcolor:red.A400
+            }
+            
+          }}
+        >
+          Clear Conversation
+        </Button>
+      </Box>
+    </Box>
+    <Box sx={{ display: "flex", flex: { md: 0.8, xs: 1, sm: 1}, flexDirection:'column', px: 3}}>
+      <Typography
+        sx={ {
+          textAlign: "center",
+          fontSize: "40px",
+          color: "white",
+          mb: 2,
+          mx: "auto",
+          fontWeight: '600',
+        } }
+      > 
+        Model - GPT 3.5 Turbo
+      </Typography>  
+      <Box
+        sx={ {
+          width: "100%",
+          height: "60vh",
+          borderRadius: 3,
+          mx: "auto",
+          display: "flex",
+          flexDirection: "column",
+          overflow: "scroll",
+          overflowX: "hidden",
+          overflowY:"auto",
+          scrollBehavior: "smooth",
+        }}
+      >
+        { chatMessages.map( ( chat ) =>
+        (<div>{ chat.content }</div> )
+        ) }
+      </Box>
+    </Box>
+  </Box>
 }
